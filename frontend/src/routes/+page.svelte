c6724294f45b585ce238d41b7e3e781abf4bf6cc
Javier Sánchez <javiersanp@gmail.com>
2023-05-01 08:12:31 +0100

    Quita sistema de mensajes en el mapa. Con el contenido del panel de información no será necesario.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,17 +3,9 @@
   import "leaflet/dist/leaflet.css"
   import { Alert } from 'flowbite-svelte';
   
-  let map, info, myLayer, messages = []
+  let map, info, myLayer
   const initialView = [[40.463667, -3.74922], 9]
   const zoomThreshold = 16
-
-  const MESSAGES = {
-    doZoom: "Haz zoom para poder editar",
-    doTask: (muncode, localId) => `Borrador edición ${muncode} ${localId}`,
-    fetchError: "Se ha producido un error al obtener el zoning.geojson",
-  }
-
-  const layerUrl = (id, label) => `http://localhost:8111/import?new_layer=true&url=https://catastro.openstreetmap.es/results/${id}/tasks/${label}.osm.gz`
   const geojsonUrl = (bounds) => `http://localhost/api/${bounds}`
 
   function createMap(container) {
@@ -27,8 +19,6 @@
       maxZoom: 20,
       minZoom: 5,
     }).addTo(m);
-
-    addMessage(MESSAGES.doZoom)
 
     return m;
   }
@@ -59,7 +49,7 @@
   }
 
   function onEachFeature({ properties: { muncode, localid } }, layer){
-    layer.on("click", () => addMessage(MESSAGES.doTask(muncode, localid)))
+    // TODO layer.on("click", () => addMessage(MESSAGES.doTask(muncode, localid)))
     layer.on("mouseover", () => layer.setStyle({ fillColor: "orange", dashArray: "5,5" }))
     layer.on("mouseout", () => myLayer.resetStyle())
   }
@@ -73,9 +63,6 @@
       const currentBounds = map.getBounds().toBBoxString()
 
       requestGeoJSON(currentBounds)
-      removeMessage(MESSAGES.doZoom)
-    } else {
-      addMessage(MESSAGES.doZoom)
     }
   }
 
@@ -96,20 +83,6 @@
     this._div.innerHTML = `Zoom: ${value || map.getZoom()}`;
   };
 
-  function addMessage(item) {
-    !messages.includes(item) && messages.push(item)
-    messages = messages
-  }
-  
-  function removeMessage(item) {
-    messages.includes(item) && messages.splice(messages.indexOf(item), 1)
-    messages = messages
-  }
-
-  function closeOverlay() {
-    messages = []
-  }
-
 </script>
 
 <svelte:window on:resize={resizeMap} />
@@ -124,15 +97,6 @@
   <div class="map w-full flex-grow z-0" use:mapAction />
 </div>
 
-{#if messages.length > 0}
-  <div class="overlay">
-    {#each messages as msg}
-      <div class="message">{msg}</div>
-    {/each}
-    <button class="overlay-close" on:click={closeOverlay}>X</button>
-  </div>
-{/if}
-
 <style>
 :global(.control) {
   padding: 6px 8px;
@@ -142,41 +106,5 @@
   border-radius: 5px;
   color: black;
   z-index: 9990 !important;
-}
-
-.overlay {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0,0,0,0.5);
-  z-index: 9999;
-  top: 0;
-  left: 0;
-  display: grid;
-  place-items: center;
-  pointer-events: none;
-}
-
-.overlay-close {
-  position: absolute;
-  z-index: 9999;
-  top: 3rem;
-  right: 3rem;
-  font-size: 3rem;
-  font-weight: 700;
-  pointer-events: auto;
-  cursor: pointer;
-  background-color: transparent;
-  border: 0;
-}
-
-.overlay-close:hover{
-  color: rgba(0,0,0,0.5);
-}
-
-.message {
-  color: white;
-  font-size: 30px;
-  font-weight: 700;
 }
 </style>
