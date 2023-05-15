f952aca86e50b4b4b11fd02af9b000a142386a19
Javier Sánchez <javiersanp@gmail.com>
2023-05-15 15:49:27 +0100

    Parchea memory leak de node en producción https://github.com/sveltejs/kit/issues/8239 https://github.com/sveltejs/kit/issues/9427 Hay que vigilar si se resuelve el problema de undici

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -3,11 +3,13 @@ import { PUBLIC_API_URL } from '$env/static/public'
 export async function handle({ event, resolve }) {
   if (!event.locals.user) {
     try {
-      const resp = await event.fetch(PUBLIC_API_URL + '/user')
-      if (resp.ok) {
-        const user = await resp.json()
-        event.locals.user = user
-      }
+      // const resp = await event.fetch(PUBLIC_API_URL + '/user')
+      // https://github.com/sveltejs/kit/issues/8239
+      const resp = await fetch(PUBLIC_API_URL + '/user', {
+        headers: {
+          cookie: 'session=' + event.cookies.get('session')
+        }
+      })
     } catch (e) {
       console.log('Falla comunicación con backend')
       console.log(e)
