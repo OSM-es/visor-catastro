bbebfa44ab527d206250ff1d816330b3a9856a7e
Javier Sanchez Portero <javiersanp@visor-catastro.cartobase.es>
2023-05-17 23:24:21 +0000

    Corrige f952aca86 sin tener que parchear la sesión. Ojo por que las llamadas a /api pasan por hook handle.

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -1,26 +1,16 @@
 import { PUBLIC_API_URL } from '$env/static/public'
+import { redirect } from '@sveltejs/kit'
 
 export async function handle({ event, resolve }) {
+  const response = await resolve(event)
   if (!event.locals.user) {
-    try {
-      // const resp = await event.fetch(PUBLIC_API_URL + '/user')
-      // https://github.com/sveltejs/kit/issues/8239
-      const resp = await fetch(PUBLIC_API_URL + '/user', {
-        headers: {
-          cookie: 'session=' + event.cookies.get('session')
-        }
-      })
-      if (resp.ok) {
-        const user = await resp.json()
-        event.locals.user = user
-      }
-    } catch (e) {
-      console.log('Falla comunicación con backend')
-      console.log(e)
+    if (event.url.pathname.startsWith('/settings')) {
+      throw redirect(302, '/')
     }
   }
-  const token = event.cookies.get('token') || ''
-  const response = await resolve(event)
-  response.headers.set('Authorization', 'Bearer ' + token)
+  if (!event.url.pathname.startsWith('/api')) {
+    const token = event.cookies.get('token') || ''
+    response.headers.set('Authorization', 'Bearer ' + token)
+  }
   return response
 }
--- frontend/src/routes/+layout.server.js
+++ frontend/src/routes/+layout.server.js
@@ -1,12 +1,16 @@
-import { redirect } from '@sveltejs/kit'
+import { PUBLIC_API_URL } from '$env/static/public'
 
-export async function load({ locals, url }) {
+export async function load({ fetch, locals, url }) {
   if (!locals.user) {
-    if (url.pathname.startsWith('/settings')) {
-      throw redirect(302, '/')
+    try {
+      const resp = await fetch(PUBLIC_API_URL + '/user')
+      if (resp.ok) {
+        locals.user = await resp.json()
+      }
+    } catch (e) {
+      console.log('Falla comunicación con backend')
+      console.log(e)
     }
   }
-  return {
-      user: locals.user
-  }
+  return { user: locals.user }
 }
