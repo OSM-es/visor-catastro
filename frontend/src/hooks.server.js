e8f2a9ab94c96c7b8c9921fe49bfa5aae8692e72
Javier Sánchez Portero <javiersanp@gmail.com>
2023-05-18 23:36:09 +0100

    No actualizaba user al entrar en settings

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -1,10 +1,26 @@
+import { PUBLIC_API_URL } from '$env/static/public'
 import { redirect } from '@sveltejs/kit'
 
 export async function handle({ event, resolve }) {
   event.locals.token = event.cookies.get('token') || ''
+  console.info(event.url.href)
+  console.info(event.locals.token)
+  if (!event.locals.user) {
+    try {
+      const resp = await event.fetch(PUBLIC_API_URL + '/user')
+      if (resp.ok) {
+        event.locals.user = await resp.json()
+        console.info(event.locals.user)
+      }
+    } catch (e) {
+      console.log('Falla comunicación con backend')
+      console.log(e)
+    }
+  }
   const response = await resolve(event)
   if (event.locals.loginRequired && !event.locals.user) {
     throw redirect(302, '/')
   }
+  console.info('----------')
   return response
 }
--- frontend/src/routes/+layout.server.js
+++ frontend/src/routes/+layout.server.js
@@ -1,16 +1,3 @@
-import { PUBLIC_API_URL } from '$env/static/public'
-
 export async function load({ fetch, locals }) {
-  if (!locals.user) {
-    try {
-      const resp = await fetch(PUBLIC_API_URL + '/user')
-      if (resp.ok) {
-        locals.user = await resp.json()
-      }
-    } catch (e) {
-      console.log('Falla comunicación con backend')
-      console.log(e)
-    }
-  }
   return { user: locals.user }
 }
