b512b66896748a28e335cd3ebe05bdfec75eb5a7
Javier Sánchez <javiersanp@gmail.com>
2023-06-04 21:50:20 +0100

    Añade DEMO al logo

--- frontend/src/routes/topbar/Logo.svelte
+++ frontend/src/routes/topbar/Logo.svelte
@@ -4,11 +4,16 @@
 
 <img
   src="{logo}"
-  class="mr-3 pt-1 h-12"
+  class="mr-1 pt-1 h-12"
   alt="openstreetmap.es"
 />
-<div class="dark:text-white">
-  <p class="text-xs font-medium h-2">visor</p>
-  <p class="text-xl font-semibold h-5 text-black dark:text-white">catastro</p>
-  <p class="text-xs text-right">OSM.es</p>
+<div class="dark:text-white flex flex-row">
+  <div class="w-5">
+    <p class="-rotate-90 text-red-600 relative start-0 top-6">DEMO</p>
+  </div>
+  <div>
+    <p class="text-xs font-medium h-2">visor</p>
+    <p class="text-xl font-semibold h-5 text-black dark:text-white">catastro</p>
+    <p class="text-xs text-right">OSM.es</p>  
+  </div>
 </div>
